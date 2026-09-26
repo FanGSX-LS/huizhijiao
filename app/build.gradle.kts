84e42a9613363a3054f2839bd1609a2b4eb69c9a
FanGSX-LS <fangshixing05@gmail.com>
2026-09-26 21:04:26 +0800

    紧急更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.gxjzy.huizhijiao"
         minSdk = 31
         targetSdk = 37
-        versionCode = 3
-        versionName = "2.2"
+        versionCode = 4
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
